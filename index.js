d4323fa44f650018dc396ba68853356bb72fb4c4
Rph <rphsoftware@gmail.com>
2021-07-15 18:55:25 +0200

    Base of app, locating steam library folders

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function startApp() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 500,
+        resizable: true,
+        backgroundColor: "#222",
+        webPreferences: {
+            devTools: true,
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    const devtools = new BrowserWindow()
+
+    win.loadFile("index.html");
+    win.webContents.setDevToolsWebContents(devtools.webContents)
+    win.webContents.openDevTools({ mode: 'detach' })
+}
+
+(async function chooch() {
+    await app.whenReady();
+    startApp();
+})();
